4c19a58034b96c07817daa3d96c1fb9d64408c7d
Alexander-Karlsson <113134577+Alexander-Karlsson@users.noreply.github.com>
2024-06-01 21:32:42 +0200

    Add app.js function to render task
    Small changes to CSS

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("hello")
+
+let list = []
+let inputButton = document.querySelector('#inputButton')
+let inputText = document.querySelector('#inputText')
+const outputList = document.querySelector('#outputList')
+
+
+console.log(outputList)
+
+inputButton.addEventListener('click', function() {
+    list.push(inputText.value)
+    inputText.value = ""
+    renderTask()
+})
+
+function renderTask() {
+    let listItem = ""
+    for (let i = 0; i < list.length; i ++) {
+        listItem += `<li>${list[i]}<input type="checkbox"></li>`
+    }
+    outputList.innerHTML = listItem
+}
