2bca61445c7e71eeb4fbc31324ea3f7ad13354d2
Alexander-Karlsson <113134577+Alexander-Karlsson@users.noreply.github.com>
2024-06-02 00:02:18 +0200

    Added clear button, animation on load and some css

--- app.js
+++ app.js
@@ -3,20 +3,46 @@ let list = []
 let inputButton = document.querySelector('#inputButton')
 let inputText = document.querySelector('#inputText')
 const outputList = document.querySelector('#outputList')
+const clearListBtn = document.querySelector('#delete-btn')
 
+let listFromLocalStorage = JSON.parse(localStorage.getItem("list"))
 
-console.log(outputList)
+if (listFromLocalStorage) {
+    list = listFromLocalStorage
+    renderTask()
+    renderClearButton()
+}
 
 inputButton.addEventListener('click', function() {
     list.push(inputText.value)
     inputText.value = ""
+    localStorage.setItem("list", JSON.stringify(list))
     renderTask()
+    renderClearButton()
+    deleteTaskBtn = true
+    
 })
 
 function renderTask() {
     let listItem = ""
     for (let i = 0; i < list.length; i ++) {
-        listItem += `<li>${list[i]}<input type="checkbox"></li>`
+        listItem += `<li>${list[i]}</li>`
     }
     outputList.innerHTML = listItem
 }
+
+clearListBtn.addEventListener('click', function() {
+    localStorage.clear()
+    list = []
+    outputList.innerHTML = ""
+    console.log(list)
+})
+
+function renderClearButton() {
+    clearListBtn.style.display = "block"
+}
+
+function hideBtn() {
+    clearListBtn.style.display = "none"
+}
+
